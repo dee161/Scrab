184d52f738b055ecd42c8fe467cc98b8a5a1e7b3
deepanshu <deepanshukumawat16@gmail.com>
2023-09-12 09:15:37 +0530

    qwe

--- node.js
+++ node.js
@@ -1,39 +1,41 @@
-const { default: puppeteer } = require("puppeteer");
-const { load } = require("cheerio");
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 const fs = require("fs/promises");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("https://www.nike.com/in/");
-  // await page.screenshot({path: "nikewebsite.png"});
-  await page.click(".pre-search-btn.ripple");
-  await page.type("#VisualSearchInput", "t-shirt");
-  await page.keyboard.press("Enter");
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://www.nike.com/in/");
 
-  await page.waitForTimeout(5000);
+    await page.click(".pre-search-btn.ripple");
+    await page.type("#VisualSearchInput", "t-shirt");
+    await page.keyboard.press("Enter");
 
-  const productData = [];
-  const $ = load(await page.content());
-  $(".product-grid__items .product-card").each((index, el) => {
-    const name = $(".product-card__title", el).text();
-    const des = $(".product-card__subtitle", el).text();
-    const image = $(el).find("img").attr("src");
-    const price = $(
-      ".product-price in__styling is--current-price css-11s12ax",
-      el
-    ).text();
-    
-    // class=""
-    productData.push({
-      name: name,
-      price: price,
-      des: des,
-      image: image,
-    });
-  });
+    await page.waitForTimeout(5000);
 
-  await browser.close();
-  console.log(productData);
-  fs.writeFile("products.json", JSON.stringify(productData, null, 4), "utf-8");
+    const pageContent = await page.content();
+    const $ = cheerio.load(pageContent);
+
+    const productData = $(".product-grid__items .product-card").map((index, el) => {
+      const name = $(".product-card__title", el).text();
+      const des = $(".product-card__subtitle", el).text();
+      const image = $(el).find("img").attr("src");
+      const price = $(".product-price.in__styling.is--current-price.css-11s12ax", el).text();
+
+      return {
+        name: name,
+        price: price,
+        des: des,
+        image: image,
+      };
+    }).get();
+
+    await browser.close();
+
+    console.log(productData);
+    await fs.writeFile("products.json", JSON.stringify(productData, null, 4), "utf-8");
+  } catch (error) {
+    console.error("Error:", error);
+  }
 })();
